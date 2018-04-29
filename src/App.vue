5d36fb64c1944a87ca960515d17ef10638da7faa
NewSimpleLife <gr390546453@gmail.com>
2018-04-30 01:28:33 +0800

    ...

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <my-accordion></my-accordion>
+  </div>
+</template>
+
+<script>
+import myAccordion  from  './my-accordion.vue'
+export default {
+  name: 'app',
+  components: {
+    myAccordion
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/my-accordion.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul @mouseenter="ulHover = true" @mouseleave="ulHover = false">
+    <li v-for="(item,index) in list" :key="index" :class="{ulHoverStyle:!ulHover}"></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'myAccordion',
+  props: {
+    list: {
+      type: Array,
+      default: function () {
+        return []
+      },
+    }
+  },
+  data () {
+    return {
+      ulHover: false,
+      ulHoverStyle: {
+        width: 100 / this.list.length + '%'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.my-accordion {
+  width: 100%;
+  height: 100%;
+}
+.my-accordion li {
+  height: 100%;
+}
+</style>
+
